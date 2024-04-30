04320668ea37671346934b1596c7fd3ea6328c52
Reginlief <102790175+reginliefx@users.noreply.github.com>
2024-04-30 19:19:36 +0700

    Create easter.lua

---
+++ easter.lua
@@ -0,0 +1,3 @@
+repeat wait() until game:IsLoaded()
+wait(3)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-69.0650711, 97.4366531, -2243.62183, 0.999944806, -1.05097655e-08, 0.0105053633, 9.60293356e-09, 1, 8.63712799e-08, -0.0105053633, -8.62656364e-08, 0.999944806)
